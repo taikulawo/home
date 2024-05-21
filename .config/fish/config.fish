bbcc2cc9c2fc72a9f91274eade9e5298a1c66758
taikulawo <qaq1362211689@gmail.com>
2024-05-21 14:35:49 +0800

    1

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -127,3 +127,31 @@ set LC_CTYPE en_US.UTF-8
 set LC_ALL en_US.UTF-8
 set RUSTUP_DIST_SERVER https://rsproxy.cn
 set RUSTUP_UPDATE_ROOT https://rsproxy.cn/rustup
+
+function up-or-search -d "Depending on cursor position and current mode, either search backward or move up one line"
+    # If we are already in search mode, continue
+    if commandline --search-mode
+        commandline -f history-search-backward
+        return
+    end
+
+    # If we are navigating the pager, then up always navigates
+    if commandline --paging-mode
+        commandline -f up-line
+        return
+    end
+
+    # We are not already in search mode.
+    # If we are on the top line, start search mode,
+    # otherwise move up
+    set lineno (commandline -L)
+
+    switch $lineno
+        case 1
+            commandline -f history-search-backward
+            history merge # <-- ADDED THIS
+
+        case '*'
+            commandline -f up-line
+    end
+end
